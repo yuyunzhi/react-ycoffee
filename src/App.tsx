bf9ad5a7cb3349b9cf205cab54f9495350dee5f7
yuyunzhi <lambert1129@foxmail.com>
2021-02-08 14:26:40 +0800

    feat: update 初始化、打包、test

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p>learn react1111</p>
       </header>
     </div>
   );
